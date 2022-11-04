d0fca3536527ffc38216ad0d75f6fbf4dfe7c1d1
Bilal Bashir <bilalbashir95@gmail.com>
2022-11-04 12:02:47 +0500

    extra miles coe 0pen bidding changes

--- app/Console/Commands/OpenBiddings.php
+++ app/Console/Commands/OpenBiddings.php
@@ -32,16 +32,42 @@ class OpenBiddings extends Command
         $client = new \GuzzleHttp\Client();
         $response = $client->request('get', 'http://128.199.227.15:5000/coe_open_bidding');
         $response = json_decode($response->getBody()->getContents());
-        $code = uniqid();
-        // dd($response);
-        $open_bidding = OpenBidding::orderBy('id' , 'desc')->first();
-        $exist = OpenBiddingParent::latest()->first();
+        if($response->message){
+            //
+        }else{
+            $code = uniqid();
+            // dd($response);
+            $open_bidding = OpenBidding::orderBy('id' , 'desc')->first();
+            $exist = OpenBiddingParent::latest()->first();
 
-        // dd($exist);
+            // dd($exist);
 
-        if($exist){
-            if($exist->end_date != $response->end_date && $exist->bidding_number != $response->bidding_number && $exist->year != $response->year && $exist->month != $response->month)
-            {
+            if($exist){
+                if($exist->end_date != $response->end_date && $exist->bidding_number != $response->bidding_number && $exist->year != $response->year && $exist->month != $response->month)
+                {
+                    $data = OpenBiddingParent::create([
+                        'month'=>$response->month,
+                        'bidding_number'=>$response->bidding_number,
+                        'end_date' => $response->end_date,
+                        'year'=>$response->year,
+                        ]);
+                    foreach($response->oneminitoring as $fuelprice){
+                        OpenBidding::create([
+                            'unique_group_id'=>$code,
+                            'parent_id' => $data->id,
+                            'grade'=>$fuelprice->category->grade,
+                            'title'=>$fuelprice->category->title,
+                            'qouta'=>$fuelprice->qouta,
+                            'QP' => $fuelprice->qouta_price,
+                            'PQP' => $fuelprice->pqp,
+                            'qouta_price_currency' => $fuelprice->qouta_price_currency,
+                            'recieved' => $fuelprice->recieved,
+                            'change_in_price' =>(float)$fuelprice->qouta_price - OpenBidding::where('unique_group_id' , $open_bidding->unique_group_id)->where('grade' ,$fuelprice->category->grade)->value('qouta_price'),
+                        ]);
+                    }
+                }
+
+            }else{
                 $data = OpenBiddingParent::create([
                     'month'=>$response->month,
                     'bidding_number'=>$response->bidding_number,
@@ -59,33 +85,12 @@ class OpenBiddings extends Command
                         'PQP' => $fuelprice->pqp,
                         'qouta_price_currency' => $fuelprice->qouta_price_currency,
                         'recieved' => $fuelprice->recieved,
-                        'change_in_price' =>(float)$fuelprice->qouta_price - OpenBidding::where('unique_group_id' , $open_bidding->unique_group_id)->where('grade' ,$fuelprice->category->grade)->value('qouta_price'),
+                        'change_in_price' =>0.0,
                     ]);
                 }
             }
-
-        }else{
-            $data = OpenBiddingParent::create([
-                'month'=>$response->month,
-                'bidding_number'=>$response->bidding_number,
-                'end_date' => $response->end_date,
-                'year'=>$response->year,
-                ]);
-            foreach($response->oneminitoring as $fuelprice){
-                OpenBidding::create([
-                    'unique_group_id'=>$code,
-                    'parent_id' => $data->id,
-                    'grade'=>$fuelprice->category->grade,
-                    'title'=>$fuelprice->category->title,
-                    'qouta'=>$fuelprice->qouta,
-                    'QP' => $fuelprice->qouta_price,
-                    'PQP' => $fuelprice->pqp,
-                    'qouta_price_currency' => $fuelprice->qouta_price_currency,
-                    'recieved' => $fuelprice->recieved,
-                    'change_in_price' =>0.0,
-                ]);
-            }
         }
+
 
         return Command::SUCCESS;
     }
